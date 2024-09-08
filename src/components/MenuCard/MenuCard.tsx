9fbe289b0353aff34c06b5c9316179d10cba6926
vietdh <vietdanghuy.95@gmail.com>
2024-09-08 16:49:50 +0700

    Create and style the buttons

--- src/components/MenuCard/MenuCard.tsx
+++ src/components/MenuCard/MenuCard.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './MenuCard.module.css';
 import { ReactComponent as HangmanTitle } from './hangman-title.svg'
+import { ReactComponent as PlayButton } from './icon-play.svg'
+import { ReactComponent as PlayButtonBackground } from './play-button-background.svg';
 
 const MenuCard: React.FC = () => {
   return (
     <div className={styles.container}>
       <HangmanTitle className={styles.title} />
-      <button className={styles.playButton}>Play</button>
+      <button className={styles.playButtonContainer}>
+        <PlayButtonBackground className={styles.playButtonBackground} />
+        <PlayButton className={styles.iconPlay} />
+      </button>
       <button className={styles.howToPlayButton}>How to Play</button>
     </div>
   );
