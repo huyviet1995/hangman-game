30317a9dd2d83322322333c2ba304852167391d6
vietdh <vietdanghuy.95@gmail.com>
2024-09-08 15:20:57 +0700

    Move Menu into separate component

---
+++ src/components/MenuCard/MenuCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './MenuCard.module.css';
+
+const MenuCard: React.FC = () => {
+  return (
+    <div className={styles.menu}>
+      <h1 className={styles.title}>Game Title</h1>
+      <button className={styles.playButton}>Play</button>
+      <button className={styles.howToPlayButton}>How to Play</button>
+    </div>
+  );
+};
+
+export default MenuCard;
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import styles from './LandingPage.module.css';
+import MenuCard from 'components/MenuCard/MenuCard';
 
 const LandingPage: React.FC = () => {
   return (
-	<div className={styles.loadingContainer}>
-	  <div className={styles.menu}>
-		<h1 className={styles.title}>Game Title</h1>
-		<button className={styles.playButton}>Play</button>
-		<button className={styles.howToPlayButton}>How to Play</button>
-	  </div>
+	<div className={styles.landingContainer}>
+		<MenuCard />
 	</div>
   );
 };
