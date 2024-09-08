94d85453dfa5d09d717ccb4f031bdb19ccccabfd
vietdh <vietdanghuy.95@gmail.com>
2024-09-08 15:52:10 +0700

    Create hangman title

--- src/components/MenuCard/MenuCard.tsx
+++ src/components/MenuCard/MenuCard.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './MenuCard.module.css';
+import { ReactComponent as HangmanTitle } from './hangman-title.svg'
 
 const MenuCard: React.FC = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Game Title</h1>
+      <HangmanTitle className={styles.title} />
       <button className={styles.playButton}>Play</button>
       <button className={styles.howToPlayButton}>How to Play</button>
     </div>
