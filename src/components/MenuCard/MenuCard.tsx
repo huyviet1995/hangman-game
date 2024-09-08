4b3039f62a49306aae505e421c839e313e7f3d72
vietdh <vietdanghuy.95@gmail.com>
2024-09-08 17:36:31 +0700

    Style how to play button text

--- src/components/MenuCard/MenuCard.tsx
+++ src/components/MenuCard/MenuCard.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import styles from './MenuCard.module.css';
-import { ReactComponent as HangmanTitle } from './hangman-title.svg'
-import { ReactComponent as PlayButton } from './icon-play.svg'
-import { ReactComponent as PlayButtonBackground } from './play-button-background.svg';
+import React from "react";
+import styles from "./MenuCard.module.css";
+import { ReactComponent as HangmanTitle } from "./hangman-title.svg";
+import { ReactComponent as PlayButton } from "./icon-play.svg";
+import { ReactComponent as PlayButtonBackground } from "./play-button-background.svg";
 
 const MenuCard: React.FC = () => {
-  return (
-    <div className={styles.container}>
-      <HangmanTitle className={styles.title} />
-      <button className={styles.playButtonContainer}>
-        <PlayButtonBackground className={styles.playButtonBackground} />
-        <PlayButton className={styles.iconPlay} />
-      </button>
-      <button className={styles.howToPlayButton}>How to Play</button>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <HangmanTitle className={styles.title} />
+            <button className={styles.playButtonContainer}>
+                <PlayButtonBackground className={styles.playButtonBackground} />
+                <PlayButton className={styles.iconPlay} />
+            </button>
+            <button className={styles.howToPlayButton}>How to Play</button>
+        </div>
+    );
 };
 
 export default MenuCard;
