f051177d0cdaa18c7f9926fe92814dd92df6bad9
vietdh <vietdanghuy.95@gmail.com>
2024-09-08 15:25:18 +0700

    Style menu container

--- src/components/MenuCard/MenuCard.tsx
+++ src/components/MenuCard/MenuCard.tsx
@@ -3,7 +3,7 @@ import styles from './MenuCard.module.css';
 
 const MenuCard: React.FC = () => {
   return (
-    <div className={styles.menu}>
+    <div className={styles.container}>
       <h1 className={styles.title}>Game Title</h1>
       <button className={styles.playButton}>Play</button>
       <button className={styles.howToPlayButton}>How to Play</button>
