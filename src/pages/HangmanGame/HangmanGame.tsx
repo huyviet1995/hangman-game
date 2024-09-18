1c1fcfa80e96e29d1961cf290bb5bab35e0c1013
vietdh <vietdanghuy.95@gmail.com>
2024-09-18 15:20:43 +0700

    Update Hangman page and fix padding stuffs

--- src/pages/HangmanGame/HangmanGame.tsx
+++ src/pages/HangmanGame/HangmanGame.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import styles from "./HangmanGame.module.css";
 import { useParams } from "react-router-dom";
+import Header from "components/Header/Header";
+import cn from "classnames";
 
 const HangmanGame: React.FC = () => {
     const { category } = useParams<{ category: string }>();
+    const handleBackClick = () => {
+        console.log("Handle back click heree.....");
+    }
+    const headerClasses = cn(styles.header, "mb-12");
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}>Hangman Game - {category}</h1>
+            <Header 
+                handleBackClick={handleBackClick}
+                className={headerClasses}
+                title={"Hangman Game - " + category}
+            />
         </div>
     );
 };
