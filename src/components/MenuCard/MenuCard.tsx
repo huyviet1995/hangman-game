0f74f52b314f9a77ded10fad8ee8cb7ab6986864
vietdh <vietdanghuy.95@gmail.com>
2024-09-21 16:58:52 +0700

    Make MenuCard component more generic

--- src/components/MenuCard/MenuCard.tsx
+++ src/components/MenuCard/MenuCard.tsx
@@ -1,49 +1,19 @@
 import React from "react";
 import styles from "./MenuCard.module.css";
-import { ReactComponent as PlayButton } from "./icon-play.svg";
-import { ReactComponent as PlayButtonBackground } from "./play-button-background.svg";
-import Typography from "components/Typography/Typography";
+// import { ReactComponent as PlayButton } from "./icon-play.svg";
+// import { ReactComponent as PlayButtonBackground } from "./play-button-background.svg";
+// import Typography from "components/Typography/Typography";
 import { useNavigate } from "react-router-dom";
 
-const MenuCard: React.FC = () => {
+interface MenuCardProps {
+    children?: React.ReactNode; 
+}
+
+const MenuCard: React.FC<MenuCardProps> = ({ children }) => {
     const navigate = useNavigate();
-    const handleHowToPlayButton = () => {
-        navigate('/tutorial');
-    }
     return (
         <div className={styles.container}>
-            <div className={styles.title}>
-                <Typography
-                    size={40}
-                    uppercase
-                    stroke={1}
-                    lineHeight={35}
-                    className={styles.textLeft}
-                >
-                    {"The"}
-                </Typography>
-                <Typography
-                    size={100}
-                    stroke={2}
-                    lineHeight={100}
-                >
-                    {"Hangman"}
-                </Typography>
-                <Typography
-                    size={40}
-                    uppercase
-                    stroke={1}
-                    lineHeight={35}
-                    className={styles.textRight}
-                >
-                    {"Game"}
-                </Typography>
-            </div>
-            <button className={styles.playButtonContainer} onClick={() => navigate('/choose-category')}>
-                <PlayButtonBackground className={styles.playButtonBackground} />
-                <PlayButton className={styles.iconPlay} />
-            </button>
-            <button className={styles.howToPlayButton} onClick={handleHowToPlayButton}>How to Play</button>
+            {children}
         </div>
     );
 };
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,13 +1,60 @@
-import React from 'react';
-import styles from './LandingPage.module.css';
-import MenuCard from 'components/MenuCard/MenuCard';
+import React from "react";
+import styles from "./LandingPage.module.css";
+import MenuCard from "components/MenuCard/MenuCard";
+import Typography from "components/Typography/Typography";
+import { ReactComponent as PlayButton } from "./icon-play.svg";
+import { ReactComponent as PlayButtonBackground } from "./play-button-background.svg";
+import { useNavigate } from "react-router-dom";
 
 const LandingPage: React.FC = () => {
-  return (
-	<div className={styles.landingContainer}>
-		<MenuCard />
-	</div>
-  );
+	const navigate = useNavigate();
+    const handleHowToPlayButton = () => {
+        navigate('/tutorial');
+    }
+    return (
+        <div className={styles.landingContainer}>
+            <MenuCard>
+                <div className={styles.title}>
+                    <Typography
+                        size={40}
+                        uppercase
+                        stroke={1}
+                        lineHeight={35}
+                        className={styles.textLeft}
+                    >
+                        {"The"}
+                    </Typography>
+                    <Typography size={100} stroke={2} lineHeight={100}>
+                        {"Hangman"}
+                    </Typography>
+                    <Typography
+                        size={40}
+                        uppercase
+                        stroke={1}
+                        lineHeight={35}
+                        className={styles.textRight}
+                    >
+                        {"Game"}
+                    </Typography>
+                </div>
+                <button
+                    className={styles.playButtonContainer}
+                    onClick={() => navigate("/choose-category")}
+                >
+                    <PlayButtonBackground
+                        className={styles.playButtonBackground}
+                    />
+                    <PlayButton className={styles.iconPlay} />
+                </button>
+                <button
+                    className={styles.howToPlayButton}
+                    onClick={handleHowToPlayButton}
+                >
+                    How to Play
+                </button>
+            </MenuCard>
+        </div>
+    );
 };
 
 export default LandingPage;
